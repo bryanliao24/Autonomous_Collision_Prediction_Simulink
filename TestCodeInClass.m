87e1003241a0c028196bcf47c3c7c05f9b629ad9
Chan Lu Liao(Bryan) <73574092+bryanliao24@users.noreply.github.com>
2023-11-13 17:23:09 -0700

    Add files via upload

---
+++ TestCodeInClass.m
@@ -0,0 +1,21 @@
+%% Test code in class
+
+clc
+clear all
+
+close all
+
+%% Conditions for rainy road
+Gain = 50000;
+InitSpeed = 25; 
+decelLim = -150;
+
+
+numSteps = 100;
+P = [0.6 0.4; 0.85 0.15];
+
+mc = dtmc(P);
+
+scenario = simulate(mc,numSteps); % 2 is HCW, 1 is LCW
+
+HilHipMethod;
